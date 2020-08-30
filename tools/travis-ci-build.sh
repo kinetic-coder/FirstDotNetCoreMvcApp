efcf5dc02f81cdcebb55d6bf652938785d30fab1
kinetic-coder <ojbullock@googlemail.com>
2020-08-30 09:32:41 +0100

    updated to include travis build

---
+++ tools/travis-ci-build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo "Executing MSBuild DLL begin command..."
+dotnet /tools/sonar/SonarScanner.MSBuild.dll begin /o:"FirstDotNetCoreMvcApp" /k:"FirstDotNetCoreMvcApp" /d:sonar.cs.vstest.reportsPaths="**/TestResults/*.trx" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=true /d:sonar.login=${SONAR_TOKEN}
+echo "Running build..."
+dotnet build FirstDotNetCoreMvcApp.csproj
+# echo "Running tests..."
+# dotnet test SonarCloud.Travis.Integration.Tests/SonarCloud.Travis.Integration.Tests.csproj --logger:trx
+echo "Executing MSBuild DLL end command..."
+dotnet ../tools/sonar/SonarScanner.MSBuild.dll end /d:sonar.login=${SONAR_TOKEN}
